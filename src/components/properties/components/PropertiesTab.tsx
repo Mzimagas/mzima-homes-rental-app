f51bcbc5d81826c8a6b6b823f8873cf4d01bf04f
Abel <a0722379217@gmail.com>
2025-09-05 14:35:18 +0300

    Fix infinite loop and maximum update depth errors in filter system

--- src/components/properties/components/PropertiesTab.tsx
+++ src/components/properties/components/PropertiesTab.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import { Button } from '../../ui'
 import Modal from '../../ui/Modal'
 import PropertyList from './PropertyList'
